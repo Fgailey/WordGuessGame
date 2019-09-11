3dfcb3167047abe04318ee0262a812922e7207f9
Foster Gailey <foster@gaileyhome.com>
2019-09-10 23:44:10 -0400

    losses, reset and letter field

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,76 +5,80 @@ var words = [
     "denver",
     "athens",
     "auburn",
-]
-        
-        //var newP = document.createElement("p");
-        var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-        //counters
-        var guessCounter = 13;
-        var wins = 0;
-        var losses = 0;
-        //this array holds the blanks and replaces them with successes 
-        var blanks = [];
-        var wrongLetters = [];
-        var chosenWordLetters = [];
-        var chosenWord = "";
-        
-            //used to reset the game after it has run an initial time
-        function reset() {
-            //randomize word selected by the computer
-            //in random word create a for loop that makes an underlined spot for each letter in word
+        ]   
 
-            //this needs to not trigger unless game is over or reset button is pressed
-            var z = Math.floor(Math.random() * words.length);
-            var chosenWord = words[z];
-            console.log(chosenWord);
-            //this will make the letters into an array for later comparison
-            chosenWordLetters = chosenWord.split('');
-
-            guessCounter = 13;
-            wrongLetters = [];
-            blanks = [];
-            letters = [];
-            // this will reset the array, the letters are taken out to avoid pressing the same letters multiple times
-            var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-
-            //this will make it so the game can run again, game stops when true
-            gameStatus = false;
-            //initialized the game to start after it has been reset
-            gameStart();
-        }
-
-            //this initiates the field for the game to start
-        function gameStart() {
-            //this needs to not trigger unless game is over or reset button is pressed
-            var z = Math.floor(Math.random() * words.length);
-            chosenWord = words[z];
-            console.log(chosenWord);
-            //this will make the letters into an array for later comparison
-            chosenWordLetters = chosenWord.split('');
-
-            guessCounter = 13;
-            wrongLetters = [];
-            blanks = [];
-            letters = [];
-            // this will reset the array, the letters are taken out to avoid pressing the same letters multiple times
-            var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
            
-            //this will run through the length of the random word and populate blanks
-            for (var x = 0; x < chosenWord.length; x++){
-                blanks.push("_");
-                $("#blank").text(blanks);
-            }
-            //changes the reset guesses back to starting value
-            $("guesses").text(guessCounter);
-            //updates the wins/losses to current total
-            $("wins").text("Wins:" + wins);
-            $("losses").text("Losses:" + losses);
+        
+    //var newP = document.createElement("p");
+    var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    //counters
+    var guessCounter = 13;
+    var wins = 0;
+    var losses = 0;
+    //this array holds the blanks and replaces them with successes 
+        //this array holds the blanks and replaces them with successes 
+    //this array holds the blanks and replaces them with successes 
+    var blanks = [];
+    var wrongLetters = [];
+    var chosenWordLetters = [];
+    var chosenWord = "";
+    
+        //used to reset the game after it has run an initial time
+    function reset() {
+        //randomize word selected by the computer
+        //in random word create a for loop that makes an underlined spot for each letter in word
 
-            $("lettersUsed").text(letters);
+        //this needs to not trigger unless game is over or reset button is pressed
+        var z = Math.floor(Math.random() * words.length);
+        var chosenWord = words[z];
+        console.log(chosenWord);
+        //this will make the letters into an array for later comparison
+        chosenWordLetters = chosenWord.split('');
+
+        guessCounter = 13;
+        wrongLetters = [];
+        blanks = [];
+        // this will reset the array, the letters are taken out to avoid pressing the same letters multiple times
+        letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
+        //this will make it so the game can run again, game stops when true
+        gameStatus = false;
+        //initialized the game to start after it has been reset
+        gameStart();
+    }
+
+    
+
+        //this initiates the field for the game to start
+    function gameStart() {
+        //this needs to not trigger unless game is over or reset button is pressed
+        var z = Math.floor(Math.random() * words.length);
+        chosenWord = words[z];
+        console.log(chosenWord);
+        //this will make the letters into an array for later comparison
+        chosenWordLetters = chosenWord.split('');
+
+        guessCounter = 13;
+        wrongLetters = [];
+        blanks = [];
+        // this will reset the array, the letters are taken out to avoid pressing the same letters multiple times
+        letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+        
+        //this will run through the length of the random word and populate blanks
+        for (var x = 0; x < chosenWord.length; x++){
+            blanks.push("_");
+            $("#blank").text(blanks);
         }
+        //changes the reset guesses back to starting value
+        $("#guesses").text(guessCounter);
+        //updates the wins/losses to current total
+        $("#wins").text("Wins:" + wins);
+        $("#losses").text("Losses:" + losses);
 
+        $("#lettersUsed").text(wrongLetters);
+    }
+    function checkWord(){
         for (var a = 0; a < letters.length; a++){
             //this for loop runs through alphabet to check user enty to the allowed chars
             if (userGuess === letters[a] && guessCounter > 0){
@@ -83,22 +87,14 @@ var words = [
                     if (userGuess === res){
                         blanks[i] = userGuess;
                         $("#blank").text(blanks);
-                        console.log(chosenWord);
+                        console.log(guessCounter);
                     }
-                    //this removes the guessed index from the array, whether correct or wrong so it cant be used again
-                    // letters.splice(userGuess);
-                    // console.log(letters)
+                    
                 }
                 
             } 
             //might need to add condition that chosenWord is not equal to the blanks
-            else if(guessCounter === 0) {
-                losses += 1;
-                $("#losses").text("Losses: " + losses);
-
-                //need to make the game reset to right after the document ready function begins
-
-            }
+            
             
             // else if(userGuess != res){
             //     //push the letter to an array that will display the used letters
@@ -108,16 +104,33 @@ var words = [
             //     $("#guesses").text("Guesses left:" + guessCounter)
             //     break;
             // } 
+                // } 
+            // } 
         }
-        
-        gameStart();
-        //used to create the blank spots
-        document.onkeyup = function(event){
-            var userGuess = event.key;
+    }
+    function winLose(){
+        if (blanks === chosenWordLetters){
+            wins += 1;
+            reset();
+        }
+        if(guessCounter === 0 || guessCounter < 0) {
+            losses += 1;
+            $("#losses").text("Losses: " + losses);
+            reset();
+            //need to make the game reset to right after the document ready function begins
+
+        }
+    }
+    gameStart();
+    //used to create the blank spots
+        document.onkeyup = function(event)
+        {
+            userGuess = event.key;
             guessCounter -= 1;
+            checkWord();
             $("guesses").text(guessCounter);
-            debugger;
-            
+            //debugger;
+            winLose();
         }
         
 
